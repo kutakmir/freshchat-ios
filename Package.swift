e56751ceac8f5ee9d9d79af8faa3789d007157f6
Miroslav Kutak <kutakmiroslav@gmail.com>
2020-11-25 01:47:12 +0100

    Target name FreshchatSDK

--- Package.swift
+++ Package.swift
@@ -4,12 +4,12 @@
 import PackageDescription
 
 let package = Package(
-    name: "freshchat-ios",
+    name: "FreshchatSDK",
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
-            name: "freshchat-ios",
-            targets: ["freshchat-ios"]),
+            name: "FreshchatSDK",
+            targets: ["FreshchatSDK"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -19,11 +19,8 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
-            name: "freshchat-ios",
+            name: "FreshchatSDK",
             dependencies: [],
-            path: "./FreshchatSDK/"),
-        .testTarget(
-            name: "freshchat-iosTests",
-            dependencies: ["freshchat-ios"]),
+            path: "./FreshchatSDK/")
     ]
 )
